bba50991ec019646216bdc7ee5ecc652c1d3cf51
Shane <ssiebler21@gmail.com>
2022-05-09 19:40:18 -0600

    added the create a manufacturer react component, up and working

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
 import ManufacturerList from './ListManufacturer';
+import ManufacturerForm from './CreateManufacturer';
 
 function App(props) {
   return (
@@ -12,6 +13,7 @@ function App(props) {
           <Route path="/" element={<MainPage />} />
           <Route path="/manufacturers">
             <Route index element={<ManufacturerList manufacturers={props.manufacturers} />} />
+            <Route path="new" element={<ManufacturerForm />} />
           </Route>
         </Routes>
       </div>
