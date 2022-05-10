c0602cfa96f2eb6da835c7ef42381a17757af5f0
Shane <ssiebler21@gmail.com>
2022-05-09 18:16:27 -0600

    added list manufacturer react componenent, had to load the data in the index.js then connnect it all in app.js

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import ManufacturerList from './ListManufacturer';
 
-function App() {
+function App(props) {
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="/manufacturers">
+            <Route index element={<ManufacturerList manufacturers={props.manufacturers} />} />
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
--- ghi/app/src/index.js
+++ ghi/app/src/index.js
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+async function loadManufacturer() {
+  let manufacturerData = [];
+  const manufacturerResponse = await fetch("http://localhost:8100/api/manufacturers/");
+
+  if (manufacturerResponse.ok) {
+    manufacturerData = await manufacturerResponse.json();
+    console.log('manufacturer data: ', manufacturerData)
+  } else {
+    console.error(manufacturerResponse);
+  }
+
+
+
+  root.render(
+    <React.StrictMode>
+      <App manufacturers={manufacturerResponse.ok ? manufacturerData : []} />
+    </React.StrictMode>
+  );
+}
+loadManufacturer();
