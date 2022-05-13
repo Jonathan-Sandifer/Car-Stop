0ae93d7a8ca88e8b0a7ebbee1c2b27597b6eaa3e
Shane <ssiebler21@gmail.com>
2022-05-13 15:06:44 -0600

    finished last react componenet for sales person history, no idea how

--- ghi/app/src/ListSpecificSalesRecords.js
+++ ghi/app/src/ListSpecificSalesRecords.js
@@ -1,33 +1,78 @@
-function SalesPersonRecordList(props) {
-  console.log('props 2222:', props.salesRecord);
+import React from 'react';
 
-  return (
-    <>
-      <h1>Sales person history</h1>
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>Sales Person</th>
-            <th>Customer</th>
-            <th>Vin</th>
-            <th>Sale Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.salesRecord.sales_records.map(salesRecord => {
-            return (
-              <tr key={salesRecord.id}>
-                <td>{salesRecord.sales_person.name}</td>
-                <td>{salesRecord.customer.name}</td>
-                <td>{salesRecord.Vin_number.vin}</td>
-                <td>{salesRecord.price}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </>
-  );
+class SalesPersonRecordList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sales_people: [],
+      sales_person: [],
+    };
+
+    this.handleChangeSalesPerson = this.handleChangeSalesPerson.bind(this);
+
+  }
+  async handleChangeSalesPerson(event) {
+    const value = event.target.value
+    const salesPersonUrl = `http://localhost:8090/api/sale_history/${value}/`
+    const salesPersonResponse = await fetch(salesPersonUrl)
+
+
+    if (salesPersonResponse.ok) {
+      const salesPersonData = await salesPersonResponse.json()
+      console.log('hello:', salesPersonData);
+      this.setState({ sales_person: salesPersonData })
+    }
+  }
+
+  async componentDidMount() {
+    const salesPeopleUrl = 'http://localhost:8090/api/salesperson/';
+    const salesPeopleResponse = await fetch(salesPeopleUrl)
+    if (salesPeopleResponse.ok) {
+      const salesPeopleData = await salesPeopleResponse.json()
+      // console.log('howdy:', salesPeopleData);
+      this.setState({ sales_people: salesPeopleData.sales_people })
+    }
+
+  }
+  render() {
+    return (
+      <>
+        <h1>Sales person history</h1>
+        <div className="mb-3">
+          <select onChange={this.handleChangeSalesPerson} placeholder="sales_people" id="sales_people" className="form-select">
+            <option value="">Choose a sales person</option>
+            {this.state.sales_people.map(sales_person => {
+              return (
+                <option key={sales_person.id} value={sales_person.id}>{sales_person.name}</option>
+              )
+            })}
+          </select>
+        </div>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>Sales Person</th>
+              <th>Customer</th>
+              <th>Vin</th>
+              <th>Sale Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.sales_person.map(sales_person => {
+              return (
+                <tr key={sales_person.id}>
+                  <td>{sales_person.sales_person.name}</td>
+                  <td>{sales_person.customer.name}</td>
+                  <td>{sales_person.Vin_number.vin}</td>
+                  <td>{sales_person.price}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </>
+    );
+  }
 }
 
-export default SalesPersonRecordList
+export default SalesPersonRecordList;
